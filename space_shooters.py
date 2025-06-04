da467669460174fdd80409dd171b31769d7dd6fb
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-06-04 16:42:40 +0000

    RST-05 completion

--- space_shooters.py
+++ space_shooters.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
 # Created by: Gustav I
 # Created on: May 28, 2025
-# This is the program for "Space shooters"
+# This is the program for "Galactic EXTRAterrestrials"
 
-import ugame
 import stage
+import ugame
+
+import constants
 
 
 def game_scene():
-    # this function is the main game scene
+    # this function is the main game game_scene
 
-    # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    # sets the backround to image 0 in the image bank
+    # and the sie (10x8 tiles of size 16x16)
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # set the background to image 0 in the image bank
-    # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+    # sets the backround to image 0 in the image bank
+    # and the sie (10x8 tiles of size 16x16)
 
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
+    # Sets the 77 tiles to the right and 66 tiles down.
 
-    # create a stage for the background to show up on
-    # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
-    # set the layers of all sprites, items show up in order
+    # set the layers, items show up in order
     game.layers = [ship] + [background]
-    # render all sprites
-    # most likely you will only render the background once per game scene
+    # render the backround and initial location of sprite list
+    # most likely you will only render backround once per scene
     game.render_block()
-    print("Welcome to Space Shooters")
 
     # repeat forever, game loop
     while True:
@@ -36,27 +39,36 @@ def game_scene():
         keys = ugame.buttons.get_pressed()
 
         if keys & ugame.K_X:
-            print("A")
+            pass
         if keys & ugame.K_O:
-            print("B")
+            pass
         if keys & ugame.K_START:
-            print("Start")
+            pass
         if keys & ugame.K_SELECT:
-            print("Select")
+            pass
         if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 2, ship.y)
-        if keys & ugame.K_LEFT:
-            ship.move(ship.x - 2, ship.y)
-        if keys & ugame.K_UP:
-            ship.move(ship.x, ship.y - 2)
-        if keys & ugame.K_DOWN:
-            ship.move(ship.x, ship.y + 2)
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 2, ship.y)
+            else:
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
 
-        # update game Logic
+        if keys & ugame.K_LEFT:
+            if ship.x >= 0:
+                ship.move(ship.x - 2, ship.y)
+            else:
+                ship.move(0, ship.y)
+
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
+        # update game logic
 
         # redraw Sprites
         game.render_sprites([ship])
+        # makes sure the ship is always on screen
         game.tick()
+        # This makes sure the ship renders at 60 Hz per frames
 
 
 if __name__ == "__main__":
