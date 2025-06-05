a82dad8caab22cde0ec82d40f378fe5acd7fb8e8
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-06-05 01:14:30 +0000

    RST-07 completion and style

--- constants.py
+++ constants.py
@@ -11,3 +11,16 @@ SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 2
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
+# new pallet for red filled text
+RED_PALETTE = (
+    b"\xff\xff\x00\x22\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+)
