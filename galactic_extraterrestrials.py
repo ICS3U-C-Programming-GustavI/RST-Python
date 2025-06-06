55dad55793ee36f0b5b88feec41089e8605f5ed8
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-06-06 00:07:31 +0000

    RST-08A-B  Completion

--- galactic_extraterrestrials.py
+++ galactic_extraterrestrials.py
@@ -5,12 +5,102 @@
 
 import stage
 import ugame
+import time
+import random
 
 import constants
 
 
+def splash_scene():
+    # this function is the splash scene function
+    # get sound ready
+    coin_sound = open("coin.wav", "rb")
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # image bank for splash scene
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # sets the backround to image 0 in the image bank
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+
+    # and the sie (10x8 tiles of size 16x16)
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+
+    # used this program to split the image into tile:
+
+    # https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+
+    background.tile(2, 2, 0)  # blank white
+
+    background.tile(3, 2, 1)
+
+    background.tile(4, 2, 2)
+
+    background.tile(5, 2, 3)
+
+    background.tile(6, 2, 4)
+
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+
+    background.tile(3, 3, 5)
+
+    background.tile(4, 3, 6)
+
+    background.tile(5, 3, 7)
+
+    background.tile(6, 3, 8)
+
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+
+    background.tile(3, 4, 9)
+
+    background.tile(4, 4, 10)
+
+    background.tile(5, 4, 11)
+
+    background.tile(6, 4, 12)
+
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+
+    background.tile(3, 5, 0)
+
+    background.tile(4, 5, 13)
+
+    background.tile(5, 5, 14)
+
+    background.tile(6, 5, 0)
+
+    background.tile(7, 5, 0)  # blank white
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = [background]
+    # render the backround and initial location of sprite list
+    # most likely you will only render backround once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        time.sleep(2.0)
+        menu_scene()
+
+
 def menu_scene():
-    # this function is the main game game_scene
+    # this function is the function for menu_scene
 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
@@ -20,7 +110,7 @@ def menu_scene():
         width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
     )
     text1.move(20, 10)
-    text1.text("MT Game Studios")
+    text1.text("Gugu Game Studios")
     text.append(text1)
 
     text2 = stage.Text(
@@ -74,9 +164,16 @@ def game_scene():
     sound.stop()
     sound.mute(False)
 
-    background = stage.Grid(image_bank_background, 10, 8)
-    # sets the backround to image 0 in the image bank
+    # sets the background to image 0 in the image bank
     # and the sie (10x8 tiles of size 16x16)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
 
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
@@ -85,11 +182,12 @@ def game_scene():
     alien = stage.Sprite(
         image_bank_sprites,
         9,
-        int(constants.SCREEN_X / 2 - constants.SPRIZE_SIZE / 2),
+        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
         16,
     )
-    # Sets the 77 tiles to the right and 66 tiles down.
 
+    # Creates a stage for the background to show up on
+    # and set the frame rate to 60 FPS
     game = stage.Stage(ugame.display, 60)
     # set the layers, items show up in order
     game.layers = [ship] + [alien] + [background]
