3cd46891c85eabe8adca11271591928006b695c9
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-06-08 01:14:16 +0000

    RST-09 Completion

--- galactic_extraterrestrials_fixed_08.py
+++ galactic_extraterrestrials_fixed_08.py
@@ -21,68 +21,42 @@ def splash_scene():
     sound.play(coin_sound)
 
     # image bank for splash scene
-    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
     # sets the backround to image 0 in the image bank
     background = stage.Grid(
-        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
-    )
-
-    # and the sie (10x8 tiles of size 16x16)
-    background = stage.Grid(
-        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+        image_bank_background, constants.SCREEN_X, constants.SCREEN_Y
     )
 
     # used this program to split the image into tile:
-
     # https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
 
     background.tile(2, 2, 0)  # blank white
-
     background.tile(3, 2, 1)
-
     background.tile(4, 2, 2)
-
     background.tile(5, 2, 3)
-
     background.tile(6, 2, 4)
-
     background.tile(7, 2, 0)  # blank white
 
     background.tile(2, 3, 0)  # blank white
-
     background.tile(3, 3, 5)
-
     background.tile(4, 3, 6)
-
     background.tile(5, 3, 7)
-
     background.tile(6, 3, 8)
-
     background.tile(7, 3, 0)  # blank white
 
     background.tile(2, 4, 0)  # blank white
-
     background.tile(3, 4, 9)
-
     background.tile(4, 4, 10)
-
     background.tile(5, 4, 11)
-
     background.tile(6, 4, 12)
-
     background.tile(7, 4, 0)  # blank white
 
     background.tile(2, 5, 0)  # blank white
-
     background.tile(3, 5, 0)
-
     background.tile(4, 5, 13)
-
     background.tile(5, 5, 14)
-
     background.tile(6, 5, 0)
-
     background.tile(7, 5, 0)  # blank white
 
     game = stage.Stage(ugame.display, constants.FPS)
@@ -119,6 +93,7 @@ def menu_scene():
     text2.move(40, 110)
     text2.text("PRESS START")
     text.append(text2)
+
     background = stage.Grid(image_bank_background, 10, 8)
     # sets the backround to image 0 in the image bank
     # and the sie (10x8 tiles of size 16x16)
@@ -186,19 +161,11 @@ def game_scene():
         16,
     )
 
-    # create list of lasers for when we shoot
-    lasers = []
-    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
-        a_single_laser = stage.Sprite(
-            image_bank_sprites, 10, constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
-        )
-        lasers.append(a_single_laser)
-
     # Creates a stage for the background to show up on
     # and set the frame rate to 60 FPS
     game = stage.Stage(ugame.display, 60)
     # set the layers, items show up in order
-    game.layers = lasers + [ship] + [alien] + [background]
+    game.layers = [ship] + [alien] + [background]
     # render the backround and initial location of sprite list
     # most likely you will only render backround once per scene
     game.render_block()
@@ -231,14 +198,10 @@ def game_scene():
         if keys & ugame.K_RIGHT != 0:
             if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
                 ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
-        else:
-            ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
 
         if keys & ugame.K_LEFT != 0:
             if ship.x > 0:
                 ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
-        else:
-            ship.move(0, ship.y)
 
         if keys & ugame.K_UP != 0:
             pass
@@ -248,27 +211,10 @@ def game_scene():
         # update game logic
         # play sound if A was just button_just_pressed
         if a_button == constants.button_state["button_just_pressed"]:
-            # fire a laser, if we have enough power (have not used up all the lasers)
-            for laser_number in range(len(lasers)):
-                if lasers[laser_number].x < 0:
-                    lasers[laser_number].move(ship.x, ship.y)
-                    sound.play(pew_sound)
-                    break
-
-        # each frame move the lasers, that have been fired up
-        for laser_number in range(len(lasers)):
-            if lasers[laser_number].x > 0:
-                lasers[laser_number].move(
-                    lasers[laser_number].x,
-                    lasers[laser_number].y - constants.LASER_SPEED,
-                )
-                if lasers[laser_number].y < constants.OFF_TOP_SCREEN:
-                    lasers[laser_number].move(
-                        constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
-                    )
+            sound.play(pew_sound)
 
         # redraw sprites
-        game.render_sprites(lasers + [ship] + [alien])
+        game.render_sprites([ship] + [alien])
         game.tick()
 
 
